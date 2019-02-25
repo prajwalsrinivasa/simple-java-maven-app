798bbc24e292a185120d6cea9d25243e39be4368
prajwalsrinivasa <prajwal1905@gmail.com>
2019-02-25 19:48:44 +0530

    test message edited

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hi prajwal you are learning the jenkins build and release";
 
     public App() {}
 
